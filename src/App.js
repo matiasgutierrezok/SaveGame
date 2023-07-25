824bdeb713bb9bd3f79a5bdc0d484f7d29056761
Matias <mati.guti95@gmail.comm>
2023-07-25 02:06:41 -0300

    add 404 error screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import { RouterProvider, createBrowserRouter
-} from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, useRouteError } from 'react-router-dom';
 import './App.scss';
 import { HomeScr } from './screens/home-scr/home-scr';
 import { createTheme, ThemeProvider } from '@mui/material';
 import { ContactMe } from './screens/contact-me/contact-me';
 import { WhatsNext } from './screens/whats-next/whats-next';
+import { Error404 } from './screens/error-404/error-404';
 
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
 });
+
+const ErrorBoundary = () => {
+  let error = useRouteError();
+  console.error(error);
+  return <Error404/>;
+}
 
 const router = createBrowserRouter([
   {
@@ -26,6 +32,7 @@ const router = createBrowserRouter([
     element: (
       <HomeScr/>
     ),
+    errorElement: <ErrorBoundary/>,
   }
 ]);
 
